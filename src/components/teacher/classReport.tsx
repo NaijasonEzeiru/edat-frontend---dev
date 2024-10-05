87f84747fff05b0f7aacda32ba4e7677850047ec
NaijasonEzeiru <jayseehe1035@gmail.com>
2024-10-05 10:56:51 +0100

    .............................

--- src/components/teacher/classReport.tsx
+++ src/components/teacher/classReport.tsx
@@ -69,7 +69,9 @@ const getAvg = (
   array
     .map((objective) => objective.score_percent)
     .filter((val) => val != -5)
-    .reduce((sum, currentValue) => sum + currentValue, 0) / array.length;
+    .reduce((sum, currentValue) => sum + currentValue, 0) /
+  array.map((objective) => objective.score_percent).filter((val) => val != -5)
+    .length;
 
 const ClassReport = () => {
   const { classId } = useParams();
@@ -214,77 +216,68 @@ const ClassReport = () => {
                   <td key={index}>
                     <HoverCard>
                       <HoverCardTrigger
-                        className="size-8 rounded-sm flex items-center justify-center m-0.5 font-semibold text-white text-xl"
+                        className="size-8 rounded-sm flex items-center justify-center m-0.5 text-white text-lg"
                         style={{
                           // width: `${+result.score_percent}%`,
                           backgroundColor: getColor(+result.score_percent),
                         }}
                       >
-                        {result.score_percent == -5 ? (
-                          "?"
-                        ) : result.score_percent > 49 ? (
-                          <p>&#10003;</p>
-                        ) : (
-                          "X"
-                        )}
+                        {result.score_percent != -5
+                          ? result.score_percent
+                          : "?"}
                         {/* </p> */}
                       </HoverCardTrigger>
-                      <HoverCardContent className="w-fit p-1">
+                      <HoverCardContent className="w-fit p-0">
                         {result.score_percent == -5 ? (
-                          "No Score"
+                          <p className="p-1">No Score</p>
                         ) : (
-                          <div className="p-1.5 grid gap-1">
-                            <p>Scrore: {result.score_percent}</p>
-                            <Sheet>
-                              <SheetTrigger asChild>
-                                <Button variant="link">View Report</Button>
-                              </SheetTrigger>
-                              <SheetContent className="sm:w-[540px] overflow-auto">
-                                <SheetHeader className="overflow-y-scroll  text-left">
-                                  <SheetTitle>Quiz Report</SheetTitle>
-                                  <SheetDescription>
-                                    {result.quizResults.map(
-                                      (val, index: number) => (
-                                        <Card className="mb-3" key={index}>
-                                          <CardHeader>
-                                            <CardTitle>
-                                              Question {index + 1}
-                                            </CardTitle>
-                                            <CardDescription>
-                                              Question: {val.question}
-                                            </CardDescription>
-                                          </CardHeader>
-                                          <CardContent>
-                                            {val.isCorrect ? (
-                                              <p className="text-green-700">
-                                                Wrong answer
-                                              </p>
-                                            ) : (
-                                              <p className="text-destructive">
-                                                Correct answer
-                                              </p>
-                                            )}
-                                            <p>
-                                              Correct option:{" "}
-                                              {val.correctOption}
+                          <Sheet>
+                            <SheetTrigger asChild>
+                              <Button variant="link">View Report</Button>
+                            </SheetTrigger>
+                            <SheetContent className="sm:w-[540px] overflow-auto">
+                              <SheetHeader className="overflow-y-scroll  text-left">
+                                <SheetTitle>Quiz Report</SheetTitle>
+                                <SheetDescription>
+                                  {result.quizResults.map(
+                                    (val, index: number) => (
+                                      <Card className="mb-3" key={index}>
+                                        <CardHeader>
+                                          <CardTitle>
+                                            Question {index + 1}
+                                          </CardTitle>
+                                          <CardDescription>
+                                            Question: {val.question}
+                                          </CardDescription>
+                                        </CardHeader>
+                                        <CardContent>
+                                          {val.isCorrect ? (
+                                            <p className="text-green-700">
+                                              Wrong answer
                                             </p>
-                                            <p>
-                                              Correct answer:{" "}
-                                              {val.correctAnswer}
+                                          ) : (
+                                            <p className="text-destructive">
+                                              Correct answer
                                             </p>
-                                            <p className="capitalize">
-                                              Your answer: {val.selectedAnswer}
-                                            </p>
-                                            <p>{val.wrongOption}</p>
-                                          </CardContent>
-                                        </Card>
-                                      )
-                                    )}
-                                  </SheetDescription>
-                                </SheetHeader>
-                              </SheetContent>
-                            </Sheet>
-                          </div>
+                                          )}
+                                          <p>
+                                            Correct option: {val.correctOption}
+                                          </p>
+                                          <p>
+                                            Correct answer: {val.correctAnswer}
+                                          </p>
+                                          <p className="capitalize">
+                                            Your answer: {val.selectedAnswer}
+                                          </p>
+                                          <p>{val.wrongOption}</p>
+                                        </CardContent>
+                                      </Card>
+                                    )
+                                  )}
+                                </SheetDescription>
+                              </SheetHeader>
+                            </SheetContent>
+                          </Sheet>
                         )}
                       </HoverCardContent>
                     </HoverCard>
