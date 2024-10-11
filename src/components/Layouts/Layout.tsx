cd45bf37f9980ef2509957c28aefc2d19c6fe1c8
NaijasonEzeiru <jayseehe1035@gmail.com>
2024-10-11 11:33:51 +0100

    ......................

---
+++ src/components/Layouts/Layout.tsx
@@ -0,0 +1,15 @@
+let _menus = [
+  {
+    id: 1,
+    name: "Profile",
+    href: `/${routerPath}/profile`,
+    icon: <UserRound />,
+  },
+  // { id: 2, name: "Skills", href: `/${routerPath}/skillset` },
+  {
+    id: 2,
+    name: "Classrooms",
+    href: `/${routerPath}/classroom`,
+    icon: <Users />,
+  },
+];
